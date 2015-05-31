9c7430d6018e3d8112d8217c600cfacc2cba38fe
Goto-Renato <r.seiji0@gmail.com>
2015-05-31 19:48:32 -0300

    package com a interface separada e modificacoes nas listagens

---
+++ src/br/usp/bbt/ui/EmprestarLivroController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.usp.bbt.ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author sushi
+ */
+public class EmprestarLivroController implements Initializable {
+    @FXML
+    private TextField nomeDeUsuario;
+    @FXML
+    private TextField id;
+    @FXML
+    private DatePicker dataRetorno;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void emprestar(ActionEvent event) {
+        System.out.println(nomeDeUsuario.getCharacters());
+        System.out.println(dataRetorno.getValue());
+        System.out.println(id.getCharacters());
+    }
+    
+}
---
+++ src/br/usp/bbt/ui/MenuPrincipalController.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.usp.bbt.ui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ * FXML Controller class
+ *
+ * @author sushi
+ */
+public class MenuPrincipalController implements Initializable  {
+    
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void cadastrarUsuario(ActionEvent event) throws IOException {
+                
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UNIFIED);
+        stage.setResizable(false);
+        stage.setTitle("Cadastro de Usuarios");
+        Parent root = FXMLLoader.load(getClass().getResource("CadastrarUsuario.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void cadastrarLivro(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UNIFIED);
+        stage.setResizable(false);
+        stage.setTitle("Cadastro de Livros");
+        Parent root = FXMLLoader.load(getClass().getResource("CadastrarLivro.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void listarUsuarios(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UNIFIED);
+        stage.setResizable(false);
+        stage.setTitle("Lista de Usuarios");
+        Parent root = FXMLLoader.load(getClass().getResource("ListarUsuarios.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void listarLivros(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UNIFIED);
+        stage.setResizable(false);
+        stage.setTitle("Lista de Livros");
+        Parent root = FXMLLoader.load(getClass().getResource("ListarLivro.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void emprestimos(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UNIFIED);
+        stage.setResizable(false);
+        stage.setTitle("Empréstimo de Livros");
+        Parent root = FXMLLoader.load(getClass().getResource("EmprestarLivro.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void devolucoes(ActionEvent event) throws IOException {
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UNIFIED);
+        stage.setResizable(false);
+        stage.setTitle("Devolução de Livros");
+        Parent root = FXMLLoader.load(getClass().getResource("ReceberLivro.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+}
